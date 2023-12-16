964fb5c740ba83aeb3d046634e08cbf64df969bb
jcisaac <isaac.jill@yahoo.com>
2023-12-16 11:19:07 -0600

    first commit

---
+++ Week2.java
@@ -0,0 +1,18 @@
+package week2;
+
+public class Week2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double costOfMilk = 3.15;
+		int thirstLevel = 5;
+
+		if (costOfMilk < 2.5 || thirstLevel > 6) {
+			System.out.println("Milk Please.");
+		} else {
+			System.out.println("No thanks.");
+		}
+
+	}
+}
